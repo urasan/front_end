37a14351883a08cc64936e59c14a7c87b6130229
dany1468 <dany1468@gmail.com>
2015-12-08 09:09:19 +0900

    ApiClient を作成した。まだ途中。

--- src/api_client.js
+++ src/api_client.js
@@ -1,3 +1,43 @@
-/**
- * Created by dan on 2015/11/28.
- */
+import request from 'superagent';
+
+class ApiClient {
+  constructor() {
+    chrome.storage.sync.get(
+      'apiUrl',
+      (item) => {
+        this._url = item.apiUrl
+      }
+    );
+  }
+
+  fetchDislikes(threadId, messageId) {
+    return new Promise((resolve, reject) => {
+      request
+        .get(`${this._url}/threads/${threadId}/messages/${messageId}/dislikes`)
+        .end(function (err, res) {
+          if (res.status === 404) {
+            reject();
+          } else {
+            resolve(JSON.parse(res.text));
+          }
+        });
+    });
+  }
+
+  dislike(threadId, messageId, userId) {
+    return new Promise((resolve, reject) => {
+      request
+        .post(`${this._url}/threads/${threadId}/messages/${messageId}/dislikes/create`)
+        .send({ user_id: userId })
+        .end(function (err, res) {
+          if (res.status === 404) {
+            reject();
+          } else {
+            resolve(JSON.parse(res.text));
+          }
+        });
+    });
+  }
+}
+
+export default new ApiClient()

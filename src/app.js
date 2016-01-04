b55b698b012b695f58ea60fbd7832d92181c4639
dany1468 <dany1468@gmail.com>
2016-01-05 08:31:23 +0900

    アクションボタンの補足には MutationObserver を単体で利用するようにした

--- src/app.js
+++ src/app.js
@@ -11,42 +11,61 @@ function getUserId() {
   return user_url.match(re)[1];
 }
 
+function setDislike(element) {
+  let actions = $(element);
+  let container = $('<li class="yj-message-list-item--action-list-item yj-message-action-list-item yj-dislike-list-item">');
+  actions.append(container);
+  // TODO threadId, messageId, userId を取り出す
+  let threadId = $(actions.parents('.yj-thread-list-item')[0]).data('thread-id');
+  let messageId = $(actions.parents('.yj-message-list-item')[0]).data('message-id');
+
+  console.log(`waitForElement load ${threadId} ${messageId}`);
+  // TODO dislike のステータスを付けるのを考えると、API アクセスする必要がある
+  //      componentWillMount でやるか悩ましい
+  ReactDOM.render(
+    <Dislike threadId={threadId} messageId={messageId} userId={share.userId}/>,
+    container[0]
+  );
+
+  // TODO sidebar は状態によっては無いことがある
+  let sidebar = $('.yj-selector-right-sidebar');
+  let sideContainer = $('<div class="yj-conversation--actions"><h2 class="yj-conversation--actions-header" role="presentation"><span role="heading" aria-level="2">Dislikes</span></h2>');
+  sidebar.append(sideContainer);
+
+  let temp = $('<div role="complementary" id="commentSidebar" class="yj-thread-sidebar">');
+  sidebar.append(temp);
+
+  let dislikeListContainer = $('<div>');
+  actions.parent().append(dislikeListContainer);
+  ReactDOM.render(
+    <DislikeList threadId={threadId} messageId={messageId} />,
+    dislikeListContainer[0]
+  );
+}
+
 $(function() {
   waitForElement('.yj-nav-menu--user-info-item', 5000).then((_element) => {
     share.userId = getUserId();
 
-    waitForElement(".yj-message-list-item--action-list.yj-actions", 5000).then((element) => {
-      let actions = $(element);
-      let container = $('<li class="yj-message-list-item--action-list-item yj-message-action-list-item yj-dislike-list-item">');
-      actions.append(container);
-      // TODO threadId, messageId, userId を取り出す
-      let threadId = $(actions.parents('.yj-thread-list-item')[0]).data('thread-id');
-      let messageId = $(actions.parents('.yj-message-list-item')[0]).data('message-id');
+    const selector = '.yj-message-list-item--action-list.yj-actions';
 
-      console.log(`${threadId} ${messageId}`);
-      // TODO dislike のステータスを付けるのを考えると、API アクセスする必要がある
-      //      componentWillMount でやるか悩ましい
-      ReactDOM.render(
-        <Dislike threadId={threadId} messageId={messageId} userId={share.userId}/>,
-        container[0]
-      );
+    let observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function (mutation) {
+        for (var i = 0; i < mutation.addedNodes.length; i++) {
+          var addedNode = mutation.addedNodes[i];
 
-      // TODO sidebar は状態によっては無いことがある
-      let sidebar = $('.yj-selector-right-sidebar');
-      let sideContainer = $('<div class="yj-conversation--actions"><h2 class="yj-conversation--actions-header" role="presentation"><span role="heading" aria-level="2">Dislikes</span></h2>');
-      sidebar.append(sideContainer);
+          if (typeof addedNode.querySelector === "function") {
+            let node = addedNode.querySelector(selector);
+            if (node) {
+              setDislike(node);
+            }
+          }
+        }
+      });
+    });
 
-      let temp = $('<div role="complementary" id="commentSidebar" class="yj-thread-sidebar">');
-      sidebar.append(temp);
-
-      let dislikeListContainer = $('<div>');
-      actions.parent().append(dislikeListContainer);
-      ReactDOM.render(
-        <DislikeList threadId={threadId} messageId={messageId} />,
-        dislikeListContainer[0]
-      );
-
-    }).catch(console.error.bind(console));
+    observer.observe(document.body, {
+      childList: true, subtree: true
+    });
   });
 });
-

97d2e1c410635d2e1d1e40327186de1fd5ce39f9
dany1468 <dany1468@gmail.com>
2016-01-09 23:52:08 +0900

    Use font-awesome-webpack instead of react-fa

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,11 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015', 'stage-0', 'react']
         }
-      }
+      },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
     ]
   }
 };

406df2ca9f388fc1552f8792bd630442319bd9bf
dany1468 <dany1468@gmail.com>
2016-01-12 22:38:50 +0900

    Change title of dislike button

--- src/dislike_button.js
+++ src/dislike_button.js
@@ -5,7 +5,7 @@ import Icon from 'react-fa';
 export default class DislikeButton extends React.Component {
   render() {
     return (
-      <a onClick={this.onClick.bind(this)} role="button" href="javascript://" title="like this message" className="yj-message-list-item--action-list-link">
+      <a onClick={this.onClick.bind(this)} role="button" href="javascript://" title="dislike this message" className="yj-message-list-item--action-list-link">
         <Icon spin name="thumbs-down" />
         <span className="yj-message-list-item--action-list-label" aria-hidden="true">{this.label()}</span>
         <span className="yj-message-list-item--action-list-acc yj-acc-hidden"> dislike this message</span>

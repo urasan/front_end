38128d561741aac3ccd1fdae71a15d3bbf49bbf6
dany1468 <dany1468@gmail.com>
2016-01-26 23:45:24 +0900

    Remove console.log

--- src/comment_item.js
+++ src/comment_item.js
@@ -2,8 +2,6 @@ import React, { Component, PropTypes } from 'react';
 
 export default class CommentItem extends Component {
   render() {
-    console.log('*******');
-    console.log(this.props.comment);
     return (
       <li>
         <span>test</span>
